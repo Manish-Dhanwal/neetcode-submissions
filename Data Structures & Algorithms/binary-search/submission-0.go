c254c641c48563d222d599cbb33449f7544cc3ae
Manish Dhanwal <73024794+Manish-Dhanwal@users.noreply.github.com>
2026-08-15 14:41:59 +0200

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.go
@@ -0,0 +1,19 @@
+func search(nums []int, target int) int {
+
+	left := 0
+	right :=len(nums)
+
+	for left < right{
+		mid := (left + right)/ 2
+
+		if nums[mid] == target{
+			return mid
+		}
+		if nums[mid] < target{
+			left = mid+1
+		}else{
+			right = mid
+		}
+	}
+return -1
+}
